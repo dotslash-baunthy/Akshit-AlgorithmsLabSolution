3de03e38584eb030c97c0d926026a09d9593e13a
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-09-20 20:19:55 +0530

    Input denominations is now in a service function

--- src/com/dsandalgo/Q2/main/Driver.java
+++ src/com/dsandalgo/Q2/main/Driver.java
@@ -2,8 +2,8 @@ package com.dsandalgo.Q2.main;
 
 import java.util.Scanner;
 
-import com.dsandalgo.Q2.services.FindDenomination;
-import com.dsandalgo.Q2.services.MergeSort;
+import com.dsandalgo.Q2.services.ProcessDenominations;
+import com.dsandalgo.Q2.services.ReverseSort;
 
 public class Driver {
     public static void main(String[] args) {
@@ -19,32 +19,23 @@ public class Driver {
         // Give notes array a size = number of notes
         notes = new int[numberOfCurrencies];
 
+        ProcessDenominations processDenominations = new ProcessDenominations();
+
         // Enter the value of the notes, each element is a note
         // Do not accept values as 0 or 1
-        System.out.print("Enter the currency denomination values: ");
-        for (int i = 0; i < numberOfCurrencies; i++) {
-            do {
-                notes[i] = scanner.nextInt();
-                if (notes[i] == 0) {
-                    System.out.println("Cannot have a donomination of value 0!");
-                } else if (notes[i] == 1) {
-                    System.out.println("Cannot have a donomination of value 1!");
-                }
-            } while (notes[i] < 2);
-        }
+        notes = processDenominations.input(notes);
 
         // Self explanatory
         System.out.print("Enter the amount you want to pay: ");
         amountToPay = scanner.nextInt();
 
         // Sort the notes in descending order
-        MergeSort mergeSort = new MergeSort();
-        mergeSort.sort(notes, 0, numberOfCurrencies - 1);
+        ReverseSort reverseSort = new ReverseSort();
+        notes = reverseSort.sort(notes, 0, numberOfCurrencies - 1);
 
         // Calculate if possible to pay accepted amount using the given currency
         // denominations
-        FindDenomination findDenomination = new FindDenomination();
-        findDenomination.findMinCombo(notes, amountToPay);
+        processDenominations.findMinCombo(notes, amountToPay);
 
         // Close scanner to prevent memory leak
         scanner.close();
